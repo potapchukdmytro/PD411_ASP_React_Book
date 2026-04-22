1d9fec37159757901b2da713bf5b3a5c42598d90
potapchukdmytro <traiget@gmail.com>
2026-04-22 17:32:23 +0300

    fix swagger

--- back/PD411_Books.API/Controllers/Test.cs
+++ back/PD411_Books.API/Controllers/Test.cs
@@ -6,6 +6,7 @@ namespace PD411_Books.API.Controllers
     [Route("api/test")]
     public class Test : ControllerBase
     {
+        [HttpGet]
         public IActionResult Get()
         {
             return Ok("Test controller");
