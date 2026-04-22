c1933895a1629599e392e132d6bbedfa5844fc50
potapchukdmytro <traiget@gmail.com>
2026-04-22 17:27:21 +0300

    Test controller

---
+++ back/PD411_Books.API/Controllers/Test.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PD411_Books.API.Controllers
+{
+    [ApiController]
+    [Route("api/test")]
+    public class Test : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok("Test controller");
+        }
+    }
+}
