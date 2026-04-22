3089ae28a7abba81653b551370895a2b72eb9f45
potapchukdmytro <traiget@gmail.com>
2026-04-22 17:01:00 +0300

    Add dockerfile

--- back/PD411_Books.API/Program.cs
+++ back/PD411_Books.API/Program.cs
@@ -127,11 +127,14 @@ var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
